a0960591c98c79f356b1abccada4ce4fc510a30d
KANDY228 <71720867+KANDY228@users.noreply.github.com>
2022-08-31 14:34:11 +0300

    ATypeName
    ATypeName

---
+++ src/main/java/MAIN/ATypeName/ATypeName.java
@@ -0,0 +1,11 @@
+package MAIN.ATypeName;
+
+public class ATypeName {
+	String ATypeName = "";
+	ATypeName(String s_arg){
+		ATypeName = s_arg;
+	}
+	public String getATypeName(){
+		return ATypeName;
+	}
+}
---
+++ src/main/java/MAIN/ATypeName/App.java
@@ -0,0 +1,11 @@
+package MAIN.ATypeName;
+
+
+public class App 
+{
+    public static void main( String[] args ){
+    	ATypeName c1;
+    	c1 = new ATypeName ("Alex");
+    	System.out.println("App: main: c1.getATypeName() ="+c1.getATypeName());
+    }
+}
